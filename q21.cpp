748ec9fd4cb2deaee07edd470cbe76fc943019ac
madara uchiha <notwhiten444@gmail.com>
2026-01-21 14:04:54 +0530

    -

---
+++ q21.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 5 ; i>0;i--){
+       for(int j = 5 ; j>0;j--){
+        if(6-i<=j)
+        cout<<" "<<j;
+    } 
+    cout<<endl;
+    }
+    return 0;
+}
