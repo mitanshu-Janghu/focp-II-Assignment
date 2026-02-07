6ec4d4113800b629e42fdd908f95f48b1b03fc7f
madara uchiha <notwhiten444@gmail.com>
2026-02-07 21:04:47 +0530

    -

--- q2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    float r ;
-    cout<<"enter radious of circle : ";
-    cin>>r;
-    cout<<endl<<"radious of circle is "<<r*r*3.14;
-    return 0;
-}
